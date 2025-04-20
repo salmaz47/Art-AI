951d0bd07b1c855b0ea920fe34d72e3cfb5987ea
Moaaz Ibrahim <75554068+moaaz-ibrahim@users.noreply.github.com>
2025-04-20 15:41:32 +0200

    Create Constants.kt

---
+++ app/src/main/java/com/salmee/artai/core/Constants.kt
@@ -0,0 +1,11 @@
+package com.salmee.artai.core
+
+object Constants {
+    const val BASE_URL = "https://276d-156-193-239-189.ngrok-free.app/api"
+    
+    object Api {
+        const val SIGNUP_ENDPOINT = "$BASE_URL/auth/signup"
+        const val LOGIN_ENDPOINT = "$BASE_URL/auth/login"
+        
+    }
+}

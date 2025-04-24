77cf192410a33c7e6f1a2812e461454f577aa7b7
Moaaz Ibrahim <75554068+moaaz-ibrahim@users.noreply.github.com>
2025-04-24 10:11:44 +0200

    Generate image API

--- app/src/main/java/com/salmee/artai/core/Constants.kt
+++ app/src/main/java/com/salmee/artai/core/Constants.kt
@@ -1,11 +1,12 @@
 package com.salmee.artai.core
 
 object Constants {
-    const val BASE_URL = "https://276d-156-193-239-189.ngrok-free.app/api"
+    const val BASE_URL = "https://dc91-196-136-5-2.ngrok-free.app/api"
     
     object Api {
         const val SIGNUP_ENDPOINT = "$BASE_URL/auth/signup"
         const val LOGIN_ENDPOINT = "$BASE_URL/auth/login"
+        const val GENERTE_IMAGE =  "$BASE_URL/images/generate"
         
     }
 }
